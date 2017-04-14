9a347d830713c2e7ebe0f6378211f57e53ab3896
Ganesh <ganeshtbk@gmail.com>
2017-04-14 20:50:10 +0530

    Tweaked the entire project
    Jsp -> Html file
    <div> replaces <table>

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ function highlight(e)
 
 function registerEvents()
 {
-	for ( var number = 1; number <= 7; number += 1 )
+	for ( var number = 1; number <= 5; number += 1 )
 	{
 		document.getElementById("day"+number).addEventListener("click",highlight,false);
 	}
 }
-
-window.addEventListener( "load", registerEvents, false );
+function loadDates() {
+   	    var i,text;
+	    var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+	    var newDate = new Date();  
+	    text = "";
+	    for (i = 1; i <= 5; i++) {
+	   	  text += "<input type=\"button\" id=\"day"+i+"\" class=\"content button\" value = \""+(newDate.getDate()+i-1)+" "+day[(newDate.getDay()+(i-1))%7]+"\"/>";
+	    }
+	    document.getElementById("date").innerHTML = text;
+		registerEvents();
+    }
+//window.addEventListener("load", registerEvents, false );
