4f99d78faa54c8c7c7b8f788834e156b00999fe1
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-04-18 18:51:15 +0530

    Removed style update using JS

--- js/script.js
+++ js/script.js
@@ -1,19 +1,5 @@
 var curr_highlight = "day1";
 
-function highlight(e)
-{
-	document.getElementById(curr_highlight).setAttribute("style","background-color:none");
-	e.target.setAttribute( "style", "background-color:lightgrey" );
-	curr_highlight = e.target.getAttribute("id");
-}
-
-function registerEvents()
-{
-	for ( var number = 1; number <= 5; number += 1 )
-	{
-		document.getElementById("day"+number).addEventListener("click",highlight,false);
-	}
-}
 function loadDates() {
     var i,text;
 	var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
@@ -23,5 +9,4 @@ function loadDates() {
 		text += "<input type=\"button\" id=\"day"+i+"\" class=\"form-item date-button\" value = \""+(newDate.getDate()+i-1)+" "+day[(newDate.getDay()+(i-1))%7]+"\"/>";
 	}
 	document.getElementById("date-buttons").innerHTML = text;
-	registerEvents();
 }
