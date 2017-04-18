e215fd6bb76c27f1c7eac7546dcdef90a608b7c7
Ganesh <ganeshtbk@gmail.com>
2017-04-18 19:33:55 +0530

    Jquery added
    Jquery added to script.js
    supporting jquery file added
    DB connected in jsp file and data displayed in table

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 var curr_highlight = "day1";
-
+var  month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+var newDate = new Date();
 function highlight(e)
 {
 	document.getElementById(curr_highlight).setAttribute("style","background-color:none");
 	e.target.setAttribute( "style", "background-color:lightgrey" );
 	curr_highlight = e.target.getAttribute("id");
+	callJSP();
 }
-
+function callJSP()
+{
+	var value = $(document.getElementById(curr_highlight)).val();
+	var hallValue = $("#hall").val();
+	$.post("seminar.jsp", { dat : value.slice(0,2), mnth : month[newDate.getMonth()] , yr : newDate.getFullYear()-2000 , hall : hallValue } , 
+	function(data,status){
+		document.getElementById("tab").rows[1].innerHTML=data;
+	});
+	$("table").show();	
+}
+function call(e)
+{
+	callJSP();
+}
 function registerEvents()
 {
 	for ( var number = 1; number <= 5; number += 1 )
 	{
 		document.getElementById("day"+number).addEventListener("click",highlight,false);
 	}
+	document.getElementById("day1").setAttribute("style","background-color:lightgrey");
+	document.getElementById("hall").addEventListener("change",call,false);
+	$("table").hide();	
 }
 function loadDates() {
     var i,text;
-	var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-	var newDate = new Date();  
 	text = "";
 	for (i = 1; i <= 5; i++) {
-		text += "<input type=\"button\" id=\"day"+i+"\" class=\"content button\" value = \""+(newDate.getDate()+i-1)+" "+day[(newDate.getDay()+(i-1))%7]+"\"/>";
+		text += "<input type=\"button\" id=\"day"+i+"\" class=\"content button\" value = \""+parseDate((newDate.getDate()+i-1))+" "+day[(newDate.getDay()+(i-1))%7]+"\"/>";
 	}
 	document.getElementById("date").innerHTML = text;
 	registerEvents();
+}
+function parseDate(date)
+{
+	if(date < 10)
+	{
+		date = '0'+date;
+	}
+	return date;
 }
--- seminar.jsp
+++ seminar.jsp
@@ -1,17 +1,37 @@
 <%@page import = "java.sql.*,java.util.*"%>
-<html>
-<head>
-	<title>Seminar</title>
-</head>
-<body>
 <%
+String dat = request.getParameter("dat");
+String month = request.getParameter("mnth");
+String yr = request.getParameter("yr");
+String hall = request.getParameter("hall");
+String parsedDate = dat+"-"+month+"-"+yr;
 Class.forName("oracle.jdbc.driver.OracleDriver");  
 Connection con = DriverManager.getConnection("jdbc:oracle:thin:seminar_hall_booking/seminar@localhost:1521:xe");
 Statement stmt = con.createStatement();
-ResultSet rs = stmt.executeQuery("Select * from seminar_hall");
+ResultSet rs = stmt.executeQuery("select staff_name , period from bookings b inner join staff_details s on s.staff_id = b.staff_id where dt = '"+parsedDate+"' and hall_number in (select hall_number from seminar_hall where branch='"+hall+"') order by period");
+int count=1,i=1;
+Integer period;
+HashMap <Integer,String> map = new HashMap<Integer,String>();
+String name;
+out.println("<td> Status </td>");
 while(rs.next()){
-out.println(rs.getString(1));	
+	period = rs.getInt(2);
+	map.put(period,rs.getString(1));
+	for(;i<=8;i++)
+	{
+		if(i==period)
+		{
+			out.println("<td>"+map.get(period)+"</td>");
+			i++;
+			break;
+		}
+		else
+			out.println("<td>Available</td>");
+	}
 }
-con.close();%>
-</body>
-</html>
+for(;i<=8;i++)
+	out.println("<td>Available</td>");
+%>
+
+
+
