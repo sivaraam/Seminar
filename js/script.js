f9ed0ab3417c5811718687c47f4fd7dfb7ec99e2
Ganesh <ganeshtbk@gmail.com>
2017-04-19 11:35:32 +0530

    Table entry modified
    An extra entry for Hours added in table

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ var curr_highlight = "day1";
 var  month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 var newDate = new Date();
-function highlight(e)
-{
+
+/*function highlight(e) {
 	document.getElementById(curr_highlight).setAttribute("style","background-color:none");
 	e.target.setAttribute( "style", "background-color:lightgrey" );
 	curr_highlight = e.target.getAttribute("id");
 	callJSP();
-}
+}*/
 
 function callJSP() {
 	var value = $(document.getElementById(curr_highlight)).val();
-	var hallValue = $("#hall").val();
+	var hallValue = $("#halls").val();
 	$.post("jsp/seminar.jsp", { dat : value.slice(0,2), mnth : month[newDate.getMonth()] , yr : newDate.getFullYear()-2000 , hall : hallValue } ,
 	function(data,status){
 		document.getElementById("status-table").rows[1].innerHTML=data;
@@ -21,19 +21,23 @@ function callJSP() {
 }
 
 function call(e) {
+	curr_highlight = e.target.getAttribute("id");
 	callJSP();
 }
 
 function registerEvents() {
-	document.getElementById("hall").addEventListener("change",call,false);
 	$("table").hide();
+	document.getElementById("halls").addEventListener("change",call,false);
+	for(i=1;i<5;i++) {
+		document.getElementById("day"+i).addEventListener("change",call,false);
+	}
 }
 
 function loadDates() {
 	date_buttons_html = "";
 	for (i = 1; i <= 5; i++) {
-    day_index = (newDate.getDay() + (i-1)) % 7;
-    curr_date = (newDate.getDate() + (i-1)) + " " + day[day_index];
+		day_index = (newDate.getDay() + (i-1)) % 7;
+		curr_date = (newDate.getDate() + (i-1)) + " " + day[day_index];
 		date_buttons_html += "<input type=\"radio\" name=\"date\" id=\"day"+i+"\" class=\"\" value = \""+curr_date+"\">"+curr_date+"</input>";
 	}
 	document.getElementById("date-buttons").innerHTML = date_buttons_html;
