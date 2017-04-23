a7ec61cb8adada1b00f8d4f3e0bfd05c66f57fcf
Ganesh <ganeshtbk@gmail.com>
2017-04-23 08:40:45 +0530

    JS refactored corresponding to index.html
    The methods except book are refactored
    Table shown after choosing hall and date

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-var curr_highlight = "day1";
+var curr_highlight = "date-1";
 var  month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 var day = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 var hours = [];
 var hallValue;
+var curr_val;
 var newDate = new Date();
 function callJSP() {
 	var value = $(document.getElementById(curr_highlight)).val();
-	hallValue = $("#halls").val();
+	hallValue = $(document.getElementById(curr_hall)).val();
+	alert(value + hallValue);
 	$.post("jsp/seminar.jsp", { dat : value.slice(0,2), mnth : month[newDate.getMonth()] , yr : newDate.getFullYear()-2000 , hall : hallValue } ,
 	function(data,status){
-		document.getElementById("status-table").rows[1].innerHTML=data;
-		$("table").show();
+		alert(data);
+		var index = data.indexOf("stop");
+		var str1 = data.slice(0,index);
+		var str2 = data.slice(index+4,-1);
+		alert(str2);
+		document.getElementById("status-table-1").rows[1].innerHTML=str1;
+		document.getElementById("status-table-2").rows[1].innerHTML=str2;
+		document.getElementById("button-container").style.display = "block";
+		document.getElementById("status-table-container").style.display = "block";
+		//$("table").show();
 		check();
 	});
 }
@@ -41,31 +51,34 @@ function book(e)
  }
 function date_change(e) {
 	curr_highlight = e.target.getAttribute("id");
-	callJSP();
 }
 
 function hall_change(e) {
+	curr_hall = e.target.getAttribute("id");
+	alert($(document.getElementById(curr_hall)).val());
+	alert("hall_change");
 	callJSP();
+
 }
 
 function registerEvents() {
-	
-	$("table").hide();
-	document.getElementById("halls").addEventListener("change",hall_change,false);
+	document.getElementById("halls-select").addEventListener("change",hall_change,false);
+	alert("sfdsdfa");
 	for(var i = 1;i <= 5; i++) {
-		document.getElementById("day"+i).addEventListener("change",date_change,false);
-	}
-	document.getElementById("Book").addEventListener("click",book,false);
+		document.getElementById("date-"+i).addEventListener("change",date_change,false);
+		document.getElementById("hall-"+i).addEventListener("change",hall_change,false);
+	}	
+	//document.getElementById("book").addEventListener("click",book,false);
 }
 
 function loadDates() {
-	date_buttons_html = "";
 	for (var i = 1; i <= 5; i++) {
 		day_index = (newDate.getDay() + (i-1)) % 7;
 		curr_date = (newDate.getDate() + (i-1)) + " " + day[day_index];
-		date_buttons_html += "<input type=\"radio\" name=\"date\" id=\"day"+i+"\" class=\"\" value = \""+curr_date+"\">"+curr_date+"</input>";
+		document.getElementById("date-"+i).setAttribute("value",curr_date);
+		document.getElementById("date-label-"+i).innerHTML = curr_date;
 	}
-	document.getElementById("date-buttons").innerHTML = date_buttons_html;
+	registerEvents();
 }
 
 function parseDate(date) {
