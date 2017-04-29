4793fa8f5af9c36df2e16db2d36f81000b444782
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-04-29 23:12:11 +0530

    Specified all strings in single quotes
     * As per guidline of Mr.Flangan, the strings
       are now quoted using single quotes
    
     More info:
    
        JavaScript: The Definitive Guide
                       - David Flanagan
        Section: 3.2.1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-﻿'use strict'
+﻿'use strict';
 /* Global constants required for Code */
-const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
-const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 
-var currSelectedDateID = "date-1";
-var currSelectedHallID = "cse";
+var currSelectedDateID = 'date-1';
+var currSelectedHallID = 'cse';
 var selectedHours = [];
 
 /************** Helper Functions *********************/
@@ -14,17 +14,17 @@ function getValues() {
   var dateValue = new Date();
   dateValue.setDate(dateValue.getDate()+dateIndex-1);  //Today's date + offset
   var hallValue = $(document.getElementById(currSelectedHallID)).val();
-  return [dateValue, hallValue]
+  return [dateValue, hallValue];
 }
 
 function showTableAndButton() {
-    document.getElementById("button-container").style.display = "block";
-    document.getElementById("status-table-container").style.display = "block";
+    document.getElementById('button-container').style.display = 'block';
+    document.getElementById('status-table-container').style.display = 'block';
 }
 
 function check() {
   $(':checkbox').change(function() {
-                        if ($(this).prop("checked"))
+                        if ($(this).prop('checked'))
                           selectedHours.push($(this).val());
                         else
                           for(var i = 0; i < selectedHours.length; i++)
@@ -43,7 +43,7 @@ function invokeDataHandler(handlerLocation, paramsObj, callBack) {
 function displaySchedule() {
   const [dateValue, hallValue] = getValues();
   invokeDataHandler(
-                     "jsp/display.jsp",
+                     'jsp/display.jsp',
                      {
                        date  : dateValue.getDate(),
                        month : months[dateValue.getMonth()],
@@ -51,11 +51,11 @@ function displaySchedule() {
                        hall  : hallValue
                      },
                      (data,status) => {
-                       var splitIndex = data.indexOf("stop");
+                       var splitIndex = data.indexOf('stop');
                        var table1Contents = data.slice(0,splitIndex);
                        var table2Contents = data.slice(splitIndex+4,-1);
-                       document.getElementById("status-table-1").rows[1].innerHTML = table1Contents;
-                       document.getElementById("status-table-2").rows[1].innerHTML = table2Contents;
+                       document.getElementById('status-table-1').rows[1].innerHTML = table1Contents;
+                       document.getElementById('status-table-2').rows[1].innerHTML = table2Contents;
                        showTableAndButton();
                        check();
                      }
@@ -69,25 +69,25 @@ function addListener(elementName,eventName,callback) {
 /************** Event Listeners **********************/
 
 function dateChangeListener(e) {
-  currSelectedDateID = e.target.getAttribute("id");
+  currSelectedDateID = e.target.getAttribute('id');
 }
 
 function hallChangeListener(e) {
-  currSelectedHallID = e.target.getAttribute("id");
+  currSelectedHallID = e.target.getAttribute('id');
   displaySchedule();
 }
 
 function bookHallListener(e) {
   const [dateValue, hallValue] = getValues();
   if(selectedHours.length === 0) {
-    confirm("Please select an hour");
+    confirm('Please select an hour');
   }
   else {
-    var staffId = prompt("Enter the your id:");
+    var staffId = prompt('Enter the your id:');
     selectedHours.forEach(
                           (hour) => {
                                       invokeDataHandler(
-                                                        "jsp/book.jsp",
+                                                        'jsp/book.jsp',
                                                         {
                                                           period   : hour,
                                                           date     : dateValue.getDate(),
@@ -102,34 +102,32 @@ function bookHallListener(e) {
                                                       );
                                     }
                           );
-    alert("The End !!!");
+    alert('The End !!!');
   }
   selectedHours = [];     //For testing
 }
 
 /************** Functions invoked during page load ********************/
 function registerEvents() {
-  addListener("date-select","change", dateChangeListener);        // For date chosen through select (small screens)
+  addListener('date-select','change', dateChangeListener);        // For date chosen through select (small screens)
   for (var i = 1; i <= 5; i++)
-    addListener("date-"+i,"change", dateChangeListener);          // For date chose through radio (bigger screens)
+    addListener('date-'+i,'change', dateChangeListener);          // For date chose through radio (bigger screens)
 
-  addListener("halls-select","change", hallChangeListener);     // For halls chosen through select (small screens)
+  addListener('halls-select','change', hallChangeListener);     // For halls chosen through select (small screens)
   for (var i = 1; i <= 7; i++)
-    addListener("hall-"+i,"change", hallChangeListener);          // For halls chose through radio (bigger screens)
+    addListener('hall-'+i,'change', hallChangeListener);          // For halls chose through radio (bigger screens)
 
-  addListener("book","click",bookHallListener);
+  addListener('book','click',bookHallListener);
 }
 
-/*
-    TODO: Dates to be loaded for select
-*/
+/*  TODO: Dates to be loaded for select  */
 function loadDates() {
   let currDate = new Date();
   for (var i = 1; i <= 5; i++) {
-    var dateAndDay = currDate.getDate() + " " + days[currDate.getDay()];
+    var dateAndDay = currDate.getDate() + ' ' + days[currDate.getDay()];
     var dateAndDayText = document.createTextNode(dateAndDay)
-    document.getElementById("date-"+i).setAttribute("value", dateAndDay);
-    document.getElementById("date-label-"+i).appendChild(dateAndDayText);
+    document.getElementById('date-'+i).setAttribute('value', dateAndDay);
+    document.getElementById('date-label-'+i).appendChild(dateAndDayText);
     currDate.setDate(currDate.getDate()+1);
   }
 }
