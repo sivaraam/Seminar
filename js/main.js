a52c050f3338dcff5696521119965c9d913e6434
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-05-01 09:34:22 +0530

    Code cleanup
      Modified formatting of code

--- js/main.js
+++ js/main.js
@@ -31,8 +31,7 @@ function check() {
                               selectedHours.splice(i,1);
                               break;
                             }
-                        }
-                       );
+                        });
 }
 
 function invokeDataHandler(handlerLocation, paramsObj, callBack) {
@@ -41,24 +40,22 @@ function invokeDataHandler(handlerLocation, paramsObj, callBack) {
 
 function displaySchedule() {
   const [dateValue, hallValue] = getValues();
-  invokeDataHandler(
-                     "jsp/display.jsp",
-                     {
-                       date  : dateValue.getDate(),
-                       month : months[dateValue.getMonth()],
-                       year  : dateValue.getFullYear()-2000,
-                       hall  : hallValue
-                     },
-                     (data,status) => {
-                       var splitIndex = data.indexOf("stop");
-                       var table1Contents = data.slice(0,splitIndex);
-                       var table2Contents = data.slice(splitIndex+4,-1);
-                       document.getElementById("status-table-1").rows[1].innerHTML = table1Contents;
-                       document.getElementById("status-table-2").rows[1].innerHTML = table2Contents;
-                       showTableAndButton();
-                       check();
-                     }
-                   );
+  invokeDataHandler("jsp/display.jsp",
+                    {
+                      date  : dateValue.getDate(),
+                      month : months[dateValue.getMonth()],
+                      year  : dateValue.getFullYear()-2000,
+                      hall  : hallValue
+                    },
+                    (data,status) => {
+                      var splitIndex = data.indexOf("stop");
+                      var table1Contents = data.slice(0,splitIndex);
+                      var table2Contents = data.slice(splitIndex+4,-1);
+                      document.getElementById("status-table-1").rows[1].innerHTML = table1Contents;
+                      document.getElementById("status-table-2").rows[1].innerHTML = table2Contents;
+                      showTableAndButton();
+                      check();
+                    });
 }
 
 function addListener(elementName,eventName,callback) {
@@ -83,24 +80,20 @@ function bookHallListener(e) {
   }
   else {
     var staffId = prompt("Enter the your id:");
-    selectedHours.forEach(
-                          (hour) => {
-                                      invokeDataHandler(
-                                                        "jsp/book.jsp",
-                                                        {
-                                                          period   : hour,
-                                                          date     : dateValue.getDate(),
-                                                          month    : months[dateValue.getMonth()],
-                                                          year     : dateValue.getFullYear()-2000,
-                                                          hall     : hallValue,
-                                                          staff_id : staffId
-                                                        },
-                                                        (data,status) => {
-                                                          alert(data);
-                                                        }
-                                                      );
-                                    }
-                          );
+    selectedHours.forEach((hour) => {
+                            invokeDataHandler("jsp/book.jsp",
+                                              {
+                                                period   : hour,
+                                                date     : dateValue.getDate(),
+                                                month    : months[dateValue.getMonth()],
+                                                year     : dateValue.getFullYear()-2000,
+                                                hall     : hallValue,
+                                                staff_id : staffId
+                                              },
+                                              (data,status) => {
+                                                alert(data);
+                                              });
+                            });
     alert("The End !!!");
   }
   selectedHours = [];     //For testing
