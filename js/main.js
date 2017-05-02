0d8008063f2871e2f4c89bca25f4b3847134d1b1
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-05-02 15:07:17 +0530

    Commented the require line temporarily

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-require('jquery');
+// require('jquery');
 /* Global constants required for Code */
 const MONTHS = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 const DAYS = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
